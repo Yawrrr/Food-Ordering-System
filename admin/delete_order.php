66b955bf39b85610c591319645805b63fd9db6b6
Tan Yun Xi <119917637+YunXiTan@users.noreply.github.com>
2024-06-25 00:10:10 +0800

    Update delete_order.php

--- admin/delete_order.php
+++ admin/delete_order.php
@@ -1,29 +1,27 @@
 <?php
 include("../connection.php");
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    echo "You must be logged in to edit an order.";
+    header("Location: ../login.php");
+    exit();
+}
 
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
     
-    // Retrieve the image file name before deleting the record
-    $result = $conn->query("SELECT image FROM menu_items WHERE id=$id");
-    $menu_item = $result->fetch_assoc();
-    $image_path = "../img/foodmenu/" . $menu_item['image'];
-
-    // Delete the image file from the server
-    if (file_exists($image_path)) {
-        unlink($image_path);
-    }
 
     // Delete the record from the database
-    $sql = "DELETE FROM menu_items WHERE id=$id";
+    $sql = "DELETE FROM orders WHERE id=$id";
     if ($conn->query($sql) === TRUE) {
         echo "Menu item deleted successfully";
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
-   
-    header("Location: all_order.php");
 
+    // Redirect back to the add_fooditem.php page
+    header("Location: all_order.php");
     exit();
 } else {
     echo "No menu item ID provided.";
